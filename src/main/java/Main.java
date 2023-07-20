82ad30b52164e881603905a10297bc8d64bb8498
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-07-21 03:14:04 +0700

    update class Main.java to create listToJson method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class Main {
@@ -12,7 +16,16 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
     }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gsonList = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        return gsonList.toJson(list, listType);
+    }
+
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
         try(CSVReader list = new CSVReader(new FileReader(fileName))) {
             ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
