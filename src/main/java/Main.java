05d5708ebf0108cfb9d9b47ea901d36b15a1fcd9
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-07-21 02:47:25 +0700

    update class Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
     }
 }
