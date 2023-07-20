6964358ae2259e13c397a96b0531bc8cbd77ebe4
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-07-21 02:54:41 +0700

    update class Main.java to create parseCSV method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
 import java.util.List;
 
 public class Main {
@@ -6,5 +12,19 @@ public class Main {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
+    }
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try(CSVReader list = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csvList = new CsvToBeanBuilder<Employee>(list)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csvList.parse();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
