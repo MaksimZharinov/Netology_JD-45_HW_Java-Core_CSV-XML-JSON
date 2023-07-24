049ce7c90dea6b7bf1851beb232615e548d0f4e8
Maksim Zharinov <minimum.cold1991@gmail.com>
2023-07-25 03:54:20 +0700

    update class Main.java to create parseXML method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,24 +5,85 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
+        //task1
+//        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+//        String fileName = "data.csv";
+//        List<Employee> list = parseCSV(columnMapping, fileName);
+//        String json = listToJson(list);
+//        writeString(json, "data.json");
+
+        //task2
+        List<Employee> list = parseXML("data.xml");
         String json = listToJson(list);
-        writeString(json);
+        writeString(json, "data2.json");
     }
 
-    private static void writeString(String json) {
-        try (FileWriter writer = new FileWriter("data.json")) {
+    private static List<Employee> parseXML(String file) {
+        List<Employee> list = new ArrayList<>();
+        long id = 0;
+        String firstName = null;
+        String lastName = null;
+        String country = null;
+        int age = 0;
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new File(file));
+            Node root = doc.getDocumentElement();
+            NodeList nodeList = root.getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element element = (Element) node;
+                    NamedNodeMap attr = element.getAttributes();
+                    for (int a = 0; a < attr.getLength(); a++) {
+                        String attrName = attr.item(a).getNodeName();
+                        String attrValue = attr.item(a).getNodeValue();
+                        switch (attrName) {
+                            case "id":
+                                id = Long.parseLong(attrValue);
+                                break;
+                            case "firstName":
+                                firstName = attrValue;
+                                break;
+                            case "lastName":
+                                lastName = attrValue;
+                                break;
+                            case "country":
+                                country = attrValue;
+                                break;
+                            case "age":
+                                age = Integer.parseInt(attrValue);
+                                break;
+                        }
+                    }
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    list.add(employee);
+                }
+            }
+            return list;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private static void writeString(String json, String file) {
+        try (FileWriter writer = new FileWriter(file)) {
             writer.write(json);
             writer.flush();
         } catch (Exception e) {
